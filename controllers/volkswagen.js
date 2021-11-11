76c2c2382fe2a8964a4a85fbbe5aa6a84bcfa60d
SESHASAIP <89412075+SESHASAIP@users.noreply.github.com>
2021-11-10 18:31:19 -0600

    Update volkswagen.js

--- controllers/volkswagen.js
+++ controllers/volkswagen.js
@@ -48,3 +48,24 @@ exports.volkswagen_view_all_Page = async function(req, res) {
         res.send(`{"error": ${err}}`); 
     }   
 }; 
+// Handle Costume create on POST. 
+exports.costume_create_post = async function(req, res) { 
+    console.log(req.body) 
+    let document = new volkswagen(); 
+    // We are looking for a body, since POST does not have query parameters. 
+    // Even though bodies can be in many different formats, we will be picky 
+    // and require that it be a json object 
+    // {"costume_type":"goat", "cost":12, "size":"large"} 
+    document.cost = req.body.cost; 
+    document.varient = req.body.varient; 
+    document.user = req.body.user; 
+    try{ 
+        let result = await document.save(); 
+        res.send(result); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
+}; 
+ 
