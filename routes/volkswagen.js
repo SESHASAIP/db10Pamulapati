c5a3d8f9df6bb9c373530746660974daa25d8f84
SESHASAIP <89412075+SESHASAIP@users.noreply.github.com>
2021-11-17 18:24:12 -0600

    added code

--- routes/volkswagen.js
+++ routes/volkswagen.js
@@ -2,6 +2,6 @@ var express = require('express');
 const costume_controlers= require('../controllers/volkswagen'); 
 var router = express.Router(); 
  
-/* GET costumes */ 
-router.get('/', costume_controlers.volkswagen_view_all_Page ); 
+/* GET detail costume page */ 
+router.get('/detail', costume_controlers.volkswagen_view_one_Page);
 module.exports = router; 
